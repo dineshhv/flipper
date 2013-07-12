97e2152663f8f7dfc0075f8c8497b0cd88f82bd5
dinesh vadivel <dinesh.kumar@asianfoodchannel.com>
2013-07-12 18:00:34 +0800

    basic commit

---
+++ js/jquery.flipper.1.0.js
@@ -0,0 +1,178 @@
+/*****************************************************
+*													 *
+*	Author: Dinesh Vadivel							 *
+*	Plugin: Jquery.flipper.1.0.0.js					 *
+*	Date:	12-07-2013								 *
+*													 *
+*													 *
+*													 *
+*													 *
+*													 *
+*													 *
+*****************************************************/
+
+(function($){
+   var Flipper = function(element, options)
+   {
+       var elem = $(element);
+       var obj = this;
+
+       // Merge options with defaults
+       var settings = $.extend({
+          target 	: 999999,
+          speed		: 1000,
+          direction	: "incr",
+          type 		: "alphanum",	//alphanum, alpha, numeric
+          sequential: "false"
+          
+       }, options || {});
+       
+       var matched, browser;
+       jQuery.uaMatch = function( ua ) {
+	   ua = ua.toLowerCase();
+	   var match = /(chrome)[ \/]([\w.]+)/.exec( ua ) ||
+        /(webkit)[ \/]([\w.]+)/.exec( ua ) ||
+        /(opera)(?:.*version|)[ \/]([\w.]+)/.exec( ua ) ||
+        /(msie) ([\w.]+)/.exec( ua ) ||
+        ua.indexOf("compatible") < 0 && /(mozilla)(?:.*? rv:([\w.]+)|)/.exec( ua ) ||
+        [];
+
+		    return {
+		        browser: match[ 1 ] || "",
+		        version: match[ 2 ] || "0"
+		    };
+		};
+
+		matched = jQuery.uaMatch( navigator.userAgent );
+		browser = {};
+		
+		if ( matched.browser ) {
+		    browser[ matched.browser ] = true;
+		    browser.version = matched.version;
+		}
+		
+		// Chrome is Webkit, but Webkit is also Safari.
+		if ( browser.chrome ) {
+		    browser.webkit = true;
+		} else if ( browser.webkit ) {
+		    browser.safari = true;
+		}
+		
+		jQuery.browser = browser;
+		
+       counter(elem, settings, matched.browser);
+      
+       animate(elem, settings, matched.browser);
+      
+       
+       // Public method
+       this.addhighlight = function()
+       {
+           
+       };
+   };
+
+   function counter(element, options, browser) 
+   {
+		
+		ID=element.attr('id');
+
+		$('#'+ID).each(function( index ) {
+			if(options.type=='alphanum')
+			{
+				textlength=options.target.length;
+				$(this).append('<ul id="myflip"></ul>');
+				for(i=0;i<textlength;i++)
+				{
+					$(this).children('#myflip').append('<li id="'+i+'" data-dig='+options.target[i]+'></li>');
+				}
+				
+			}
+			else if(options.type=='alpha')
+			{
+				$(this).html(options.target);	
+			}
+			else if(options.type=='numeric')
+			{
+				length=options.target.toString().length;
+				console.log(length);
+				$(this).html(options.target);	
+			}
+
+		});
+	    	
+   }
+   function animate(element, options, browser) 
+   {
+   		ID=element.attr('id');
+   		textlength=options.target.length;
+   		if(options.type=='alphanum')
+		{
+			var arr = ['1','2','3','4','5','6','7','8','9','0','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		}
+		else if(options.type=='alpha')
+		{
+			var arr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+		}
+		else if(options.type=='numeric')
+		{
+			var arr = [1,2,3,4,5,6,7,8,9,0];
+		}
+		i=0;
+		var flag=[];
+		for(ins=0;ins<=textlength;ins++)
+		{
+			flag[ins]=0;
+		}
+		
+   		(function tick(){
+   			
+	   		$('#'+ID+' #myflip li').each(function( index ) {
+	   			crid=$(this).attr('id');
+	   			
+				digit=$(this).data('dig').toString();
+				if(flag[crid]!=1)
+				{
+					//console.log(crid);
+					if(digit.toLowerCase()==arr[i])
+					{
+						$('#'+crid).text(arr[i]);
+						myv=parseInt(crid);
+						
+						flag[myv]=1;
+					   
+					  
+					}
+					else
+					{	
+					    $('#'+crid).text(arr[i]);
+					}
+				}
+			});
+	   		
+	   		i++;
+   		var time=setTimeout(tick, 100);
+		})();
+		//window.clearTimeout(time);
+   }
+  function isAlphanumeric( str ) {
+	  return /^[0-9]+$/.test(str);
+  }
+  
+   $.fn.flipper = function(options)
+   {
+       return this.each(function()
+       {
+           var element = $(this);
+          
+           // Return early if this element already has a plugin instance
+           if (element.data('flipper')) return;
+
+           // pass options to plugin constructor
+           var relocate = new Flipper(this, options);
+          
+           // Store plugin object in this element's data
+           element.data('flipper', relocate);
+       });
+   };
+})(jQuery);
